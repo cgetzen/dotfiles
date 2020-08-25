400c1ce585bfcc340580a96ebf7ff8d7230b87e7
cgetzen <charliegetzenlc@gmail.com>
2020-08-25 13:06:40 -0500

    fix echo in brew_manager

--- brew_manager.sh
+++ brew_manager.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 echo "# Update"
 /usr/local/bin/brew update
-echo "\n\n# Upgrade"
+echo -e "\n\n# Upgrade"
 /usr/local/bin/brew upgrade
 /usr/local/bin/brew cask upgrade
 
-echo "\n\n# Cleanup"
+echo -e "\n\n# Cleanup"
 /usr/local/bin/brew cleanup -s
 
-echo "\n\n# Diagnostics"
+echo -e "\n\n# Diagnostics"
 /usr/local/bin/brew doctor
 /usr/local/bin/brew missing
 /usr/local/bin/brew outdated
